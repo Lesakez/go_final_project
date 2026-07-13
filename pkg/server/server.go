96145b3b9ef67d6b7ae3e821c42f82cabf3388c5
Viktor Vorovko <lesake@MacBook-Air-Viktor.local>
2026-07-13 16:33:22 +0400

    Add configurable port via TODO_PORT and startup log

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,11 +1,21 @@
-package server 
+package server
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+const defaultPort = "7540"
 
 func Run() error {
-	
+	port := os.Getenv("TODO_PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
 	http.Handle("/", http.FileServer(http.Dir("web")))
 
-	return http.ListenAndServe(":7540", nil)
-	
+	log.Printf("Server started on http://localhost:%s", port)
+	return http.ListenAndServe(":"+port, nil)
 }
