6e8fd690929e2448fdde09c3e90f1e2188c811f5
Viktor Vorovko <lesake@MacBook-Air-Viktor.local>
2026-07-18 02:05:57 +0400

    Add SQLite database initialization

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
-package main 
+package main
 
 import (
 	"log"
+	"os"
 
+	"go_final_project/pkg/db"
 	"go_final_project/pkg/server"
 )
 
 func main() {
+	dbFile := os.Getenv("TODO_DBFILE")
+	if dbFile == "" {
+		dbFile = "scheduler.db"
+	}
+
+	if err := db.Init(dbFile); err != nil {
+		log.Fatal(err)
+	}
 
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
-
 }
